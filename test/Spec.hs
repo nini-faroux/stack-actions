4db84943a180e951a230d2a33448e6760eb125fb
nini-faroux <nfarrel1@tcd.ie>
2021-06-19 17:04:02 +0100

    small change to trigger action

--- test/Spec.hs
+++ test/Spec.hs
@@ -4,5 +4,6 @@ import Lib
 main :: IO ()
 main = hspec $
   describe "square" $
-    it "squares an integer" $
+    it "squares an integer" $ do
       square 2 `shouldBe` 4
+      square 3 `shouldBe` 9
